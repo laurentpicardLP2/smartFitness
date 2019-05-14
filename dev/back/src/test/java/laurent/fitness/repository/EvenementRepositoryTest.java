7af016f4da74aa77f1cdc145bc30f7e227d8a218
laurent <laurent.picard.LP2@gmail.com>
2019-05-14 22:38:06 +0200

    implements junit test

--- dev/back/src/test/java/laurent/fitness/repository/EvenementRepositoryTest.java
+++ dev/back/src/test/java/laurent/fitness/repository/EvenementRepositoryTest.java
@@ -1,5 +1,76 @@
 package laurent.fitness.repository;
 
-public class EvenementRepositoryTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import laurent.fitness.model.Evenement;
+import laurent.fitness.model.FacilityCategory;
+
+
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=Replace.NONE)
+public class EvenementRepositoryTest {
+	
+	@Autowired
+	private EvenementRepository evenementRepo;
+	
+
+	@Autowired
+	private TestEntityManager testEntityManager;
+	
+	
+	@Test
+	@WithMockUser(roles={"ADMIN"})
+	public void testFindByIdEvenement() {
+		Evenement ouvSmartFitness = evenementRepo.findByIdEvenement(1);
+		assertThat(ouvSmartFitness.getTitleEvt()).isEqualTo("Evt 1");
+	}
+	
+	
+	@Test
+	@WithMockUser(roles={"ADMIN"})
+	public void testFindByIdMaxEvenement() {
+		assertThat(evenementRepo.findByIdMaxEvenement()).isEqualTo(4);
+	}
+	
+	
+	@Test
+	@WithMockUser(roles={"ADMIN"})
+	public void testFindByEvenementInProgress() {
+		assertThat((evenementRepo.findByEvenementInProgress()).size()).isEqualTo(3);
+	}
+	
+	@Test
+	@WithMockUser(roles={"ADMIN"})
+	public void testFindByEvenementInSlotTime() {
+		assertThat((evenementRepo.findByEvenementInSlotTime()).size()).isEqualTo(2);
+	}
+	
+	@Test
+	@WithMockUser(roles={"ADMIN"})
+	public void testSaveEvenement() {
+		Evenement savedEvenement = testEntityManager.persistFlushFind(new Evenement("Evt 5"));
+		Evenement evt = evenementRepo.findByIdEvenement(5);
+		assertThat(evt.getTitleEvt()).isEqualTo(savedEvenement.getTitleEvt());
+	}
+	
+	@Test
+	@WithMockUser(roles={"ADMIN"})
+	public void testDeleteEvenement() {
+		Evenement evt = evenementRepo.findByIdEvenement(5);
+		evenementRepo.delete(evt);
+		assertThat(evenementRepo.findByIdEvenement(5)).isNull();
+	}
 }
--- dev/back/src/test/java/laurent/fitness/services/EvenementServiceImplTest.java
+++ dev/back/src/test/java/laurent/fitness/services/EvenementServiceImplTest.java
@@ -10,15 +10,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import laurent.fitness.model.Evenement;
-import laurent.fitness.model.Facility;
 import laurent.fitness.repository.EvenementRepository;
-import laurent.fitness.repository.FacilityCategoryRepository;
-import laurent.fitness.repository.FacilityRepository;
-import laurent.fitness.repository.MaintenanceOperationRepository;
-import laurent.fitness.repository.RoomRepository;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EvenementServiceImplTest {
@@ -86,5 +82,23 @@ public class EvenementServiceImplTest {
 		Evenement savedOuvSmartFitness = evenementService.saveEvenement(ouvSmartFitness);
 
 		assertThat(savedOuvSmartFitness.getTitleEvt()).isEqualTo("Ouverture Smart Fitness");
+	}
+	
+	@Test
+	public void updateEvenement() {
+		Evenement ouvSmartFitness = new Evenement("Ouverture Smart Fitness");
+		ouvSmartFitness.setTitleEvt("Ouverture Smart Fitness le 01 juillet 2019 à 8h00");
+		given(evenementRepo.save(ouvSmartFitness)).willReturn(new Evenement("Ouverture Smart Fitness le 01 juillet 2019 à 8h00"));
+
+		Evenement updatedOuvSmartFitness = evenementService.saveEvenement(ouvSmartFitness);
+		assertThat(updatedOuvSmartFitness.getTitleEvt()).isEqualTo("Ouverture Smart Fitness le 01 juillet 2019 à 8h00");
+	}
+	
+	@Test
+	public void deleteEvenement() {
+		Evenement ouvSmartFitness = new Evenement("Ouverture Smart Fitness");
+		evenementRepo.delete(ouvSmartFitness);
+
+		Mockito.verify(evenementRepo).delete(ouvSmartFitness);
 	}
 }
